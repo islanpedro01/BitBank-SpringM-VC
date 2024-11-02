0a1d30fabb8c0720c93ed610322b33b3424b0e5c
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2024-11-02 15:54:24 -0300

    Correntista Done. Starting ContaController

--- src/main/java/br/edu/ifpb/pweb2/bitbank/controller/CorrentistaController.java
+++ src/main/java/br/edu/ifpb/pweb2/bitbank/controller/CorrentistaController.java
@@ -17,12 +17,28 @@ public class CorrentistaController {
     @RequestMapping("/form")
     public String getForm(Correntista correntista, Model model) {
         model.addAttribute("correntista", correntista);
+
         return "correntistas/form";
-
-
 
     }
 
+    @RequestMapping("/save")
+    public String save(Correntista correntista, Model model) {
+        String mensagem;
+    if (correntista.getNome().isEmpty() || correntista.getNome().length() > 50) {
+        mensagem = "O nome é obrigatório e deve ter até 50 caracteres!";
+        model.addAttribute("mensagem", mensagem);
+        return "correntistas/form";
+    }
+    if (correntista.getEmail().isEmpty() || correntista.getSenha().isEmpty()) {
+        mensagem = "Os campos E-MAIL e SENHA são obrigatórios!";
+        model.addAttribute("mensagem", mensagem);
+        return "correntistas/form";
+        }
+        correntistaRepository.save(correntista);
+        model.addAttribute("correntistas", correntistaRepository.findAll());
+        return "correntistas/list";
+    }
 
 
    }
